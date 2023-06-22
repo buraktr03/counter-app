47d5f3a602faab7d0325f6b15bc1357e375bdee6
buraktr03 <turanburak@gmail.com>
2023-06-23 00:28:45 +0300

    js added

---
+++ script.js
@@ -0,0 +1,15 @@
+const increaseBtn = document.querySelector(".increase")
+const decreaseBtn = document.querySelector(".decrease")
+const number = document.getElementById("number")
+
+let initialNumber = 0
+
+increaseBtn.addEventListener("click",()=>{
+    initialNumber++
+    number.innerHTML = `${initialNumber}`
+})
+
+decreaseBtn.addEventListener("click",()=>{
+    initialNumber--
+    number.innerHTML = `${initialNumber}`
+})
